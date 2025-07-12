8a6f03557d1ba4fe880a5f360606bc63e470fb98
uddithmachiraju <uddithmachiraju@gmail.com>
2025-07-12 15:41:08 +0000

    creation of iam roles for lambda

---
+++ app/iam/role.py
@@ -0,0 +1,44 @@
+import boto3 
+import json
+
+def create_iam_role(rolename):
+    iam_client = boto3.client("iam")
+
+    # Define policy to the Role
+    assume_role_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+
+    # Create an IAM Role
+    try:
+        response = iam_client.create_role(
+            RoleName = rolename,
+            AssumeRolePolicyDocument = json.dumps(assume_role_policy),
+            Description = "IAM Role for Lambda Execution"
+        )
+
+        # Add policies 
+        # 1. CloudWatch 
+        iam_client.attach_role_policy(
+            RoleName = rolename,
+            PolicyArn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+        )
+
+        # 2. S3 Read only access 
+        iam_client.attach_role_policy(
+            RoleName = rolename,
+            PolicyArn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+        )
+        print(f"IAM Role Created with name: {rolename}") 
+        return response
+    except Exception as e:
+        print(f"Something Intrupted: {e}") 

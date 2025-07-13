15a1604d8b607a75b72ecd797985284553137591
uddithmachiraju <uddithmachiraju@gmail.com>
2025-07-13 07:17:05 +0000

    Creation of lambda function

---
+++ app/lambda_func/setup_lambda.py
@@ -0,0 +1,25 @@
+import boto3  
+
+def create_lambda_function(function_name, region, role_arn, zip_path, handler, runtime):
+    lambda_client = boto3.client("lambda", region_name = region) 
+
+    # Need to pass the binary code to lambda 
+    with open(zip_path, "rb") as file:
+        zipped_code = file.read() 
+
+    # Create a lambda function 
+    try:
+        response = lambda_client.create_function(
+            FunctionName = function_name,
+            Runtime = runtime, 
+            Role = role_arn, 
+            Handler = handler,
+            Code = {
+                "ZipFile": zipped_code
+            },
+            Timeout = 30,
+            MemorySize = 128, 
+        ) 
+        print(f"Lambda Function Created: {function_name}")
+    except Exception as e:
+        print(f"Exception: {e}") 
--- deploy/deploy_lambda.py
+++ deploy/deploy_lambda.py
@@ -1,13 +1,22 @@
+import time 
 from app.s3.setup_buckets import create_bucket_if_not_exists
-from app.iam.role import create_iam_role
+from app.iam.role import create_iam_role 
+from app.lambda_func.setup_lambda import create_lambda_function
 
 bucket_1 = "bucket-1-5027" 
 bucket_2 = "bucket-2-5027"
+region = "ap-south-1" 
 
 def deploy(filename = "tasks.txt", key = "tasks.txt"):
-    create_bucket_if_not_exists(bucket_name = bucket_1, region = "ap-south-1") 
-    create_bucket_if_not_exists(bucket_name = bucket_2, region = "ap-south-1") 
-    create_iam_role(rolename = "lambda-s3-execution-role") 
+    bucket_1_response = create_bucket_if_not_exists(bucket_name = bucket_1, region = region) 
+    bucket_2_response = create_bucket_if_not_exists(bucket_name = bucket_2, region = region)
+    iam_role_response = create_iam_role(rolename = "lambda-s3-execution-role", region = region) 
+    # print(iam_role_response["Role"]["Arn"]) 
+    print("Waiting 60 seconds for IAM role to propagate...")
+    time.sleep(60) 
+    create_lambda_function(function_name = "AWSIPP", region = region, 
+                           role_arn = iam_role_response["Role"]["Arn"], zip_path = "app/lambda_func/lambda_func.zip", handler = "handler.lambda_handler", 
+                           runtime = "python3.12")
 
 if __name__ == "__main__":
     deploy()

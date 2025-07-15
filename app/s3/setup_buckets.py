89adad776037f9c3b8e0af70df1c51debaabbbfe
uddithmachiraju <uddithmachiraju@gmail.com>
2025-07-15 04:52:40 +0000

    Added s3 bucket policy to allow lambda access

--- app/s3/setup_buckets.py
+++ app/s3/setup_buckets.py
@@ -1,6 +1,7 @@
 # File for creating the buckets and uploading files to buckets
 
 import boto3 
+import json 
 
 def if_exists(client, bucket_name: str): 
     try: 
@@ -8,6 +9,31 @@ def if_exists(client, bucket_name: str):
         return True
     except:
         return False
+    
+def attach_bucket_policy(client, bucket_name, role_arn):
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AllowLambdaGetAndPut",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": role_arn
+                },
+                "Action": ["s3:GetObject", "s3:PutObject"],
+                "Resource": f"arn:aws:s3:::{bucket_name}/*"
+            }
+        ]
+    }
+
+    try: 
+        client.put_bucket_policy(
+            Bucket = bucket_name,
+            Policy = json.dumps(bucket_policy) 
+        )
+        print(f"Attached policy to the Bucket: {bucket_name}")
+    except Exception as e:
+        print(f"Failed to attach bucket policy: {e}") 
 
 def create_bucket_if_not_exists(bucket_name: str, region: str): 
     s3_client = boto3.client("s3", region_name = region) 
